b81fc7132ca96ba308aa140f8f73905548e518ab
Cardinal Ncube <52386656+cardyy@users.noreply.github.com>
2023-10-19 20:36:37 +0200

    Add files via upload

---
+++ build/precache-manifest.96bbb1311bb589a03638d85cdcf10598.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ae9b4a84c83e31d28d6caf0e91daef",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "69541c7ab4e5c645becb",
+    "url": "/frontend/static/css/main.bfeca261.chunk.css"
+  },
+  {
+    "revision": "eca954b75f20e1a00252",
+    "url": "/frontend/static/js/2.ab0c86fa.chunk.js"
+  },
+  {
+    "revision": "b6ae303aff320f169c3c376929e07840",
+    "url": "/frontend/static/js/2.ab0c86fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69541c7ab4e5c645becb",
+    "url": "/frontend/static/js/main.2d5b2d19.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/frontend/static/js/main.2d5b2d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd736ec3a17f1866f598",
+    "url": "/frontend/static/js/runtime-main.6501fd29.js"
+  },
+  {
+    "revision": "3e4bf91accad8e7a242fa7b0ef027e03",
+    "url": "/frontend/static/media/0B.3e4bf91a.png"
+  },
+  {
+    "revision": "af20f9cc7870d816819a77a2fdf67954",
+    "url": "/frontend/static/media/0W.af20f9cc.png"
+  },
+  {
+    "revision": "d576ecc30052038b4a0d67c1e5369f06",
+    "url": "/frontend/static/media/AS.d576ecc3.png"
+  },
+  {
+    "revision": "f2ad1a8052eaf7199261340d9e00ee93",
+    "url": "/frontend/static/media/C.f2ad1a80.png"
+  },
+  {
+    "revision": "54b39b642639883578ec8e14c3d8e00d",
+    "url": "/frontend/static/media/H.54b39b64.png"
+  },
+  {
+    "revision": "74fe77f9f57131165427820af5ba1bd2",
+    "url": "/frontend/static/media/JC.74fe77f9.png"
+  },
+  {
+    "revision": "830f99ea2ac7ba54e7b37b53e2b43d6b",
+    "url": "/frontend/static/media/JD.830f99ea.png"
+  },
+  {
+    "revision": "e9c183c04f9542bca2a8a493d16e7ed9",
+    "url": "/frontend/static/media/JH.e9c183c0.png"
+  },
+  {
+    "revision": "4e4c16af6f6194dbf582ab52b4c25054",
+    "url": "/frontend/static/media/JS.4e4c16af.png"
+  },
+  {
+    "revision": "f079c963ddb85c94a8671858596142fb",
+    "url": "/frontend/static/media/KC.f079c963.png"
+  },
+  {
+    "revision": "5cf70659cbd24ed931194f6056959a52",
+    "url": "/frontend/static/media/KD.5cf70659.png"
+  },
+  {
+    "revision": "429a41415dbb4f91a3af8d51417173e7",
+    "url": "/frontend/static/media/KH.429a4141.png"
+  },
+  {
+    "revision": "310939fd0ea50754d11f171445cbf699",
+    "url": "/frontend/static/media/KS.310939fd.png"
+  },
+  {
+    "revision": "c115483250e824f128fa00fa64620478",
+    "url": "/frontend/static/media/QC.c1154832.png"
+  },
+  {
+    "revision": "bfd5985c0ecd28c34fe36f506061885e",
+    "url": "/frontend/static/media/QD.bfd5985c.png"
+  },
+  {
+    "revision": "0a950a9d60474c8162ac6bae9033e4e1",
+    "url": "/frontend/static/media/QH.0a950a9d.png"
+  },
+  {
+    "revision": "a911c11e5dabafad0dfeabc81f5ac110",
+    "url": "/frontend/static/media/QS.a911c11e.png"
+  },
+  {
+    "revision": "9ffb89c834be55b833b1882c011cd0e9",
+    "url": "/frontend/static/media/S.9ffb89c8.png"
+  },
+  {
+    "revision": "0ac5b79956d7712595bd10f7effb7226",
+    "url": "/frontend/static/media/TC.0ac5b799.png"
+  },
+  {
+    "revision": "eb2740e1f33d61e64c58677a5426088b",
+    "url": "/frontend/static/media/deck.eb2740e1.png"
+  },
+  {
+    "revision": "9e662ccb8150044fb619e26e76841b39",
+    "url": "/frontend/static/media/four.9e662ccb.png"
+  },
+  {
+    "revision": "515d1415bc0f028756948d26fa9db532",
+    "url": "/frontend/static/media/home.515d1415.jpg"
+  },
+  {
+    "revision": "ed24f1adb825b05ac7840c48999ca1b6",
+    "url": "/frontend/static/media/next.ed24f1ad.png"
+  },
+  {
+    "revision": "76264bc3ea2d3e123f193c440302de99",
+    "url": "/frontend/static/media/table.76264bc3.png"
+  },
+  {
+    "revision": "368755748bd1004078dfa1a44d59aa55",
+    "url": "/frontend/static/media/text.36875574.png"
+  },
+  {
+    "revision": "d8696bf3c0cef0a3cccb140960aa4f42",
+    "url": "/frontend/static/media/three.d8696bf3.png"
+  },
+  {
+    "revision": "0913e3faa021822d3b5470fdd2ae7f4b",
+    "url": "/frontend/static/media/two.0913e3fa.png"
+  }
+]);
