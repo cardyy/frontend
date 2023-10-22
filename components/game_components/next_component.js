bf2c50920546b0fb395f39aab16de0d04af0a0bf
Cardinal Ncube <52386656+cardyy@users.noreply.github.com>
2023-10-22 12:13:41 +0200

    Add files via upload

--- components/game_components/next_component.js
+++ components/game_components/next_component.js
@@ -1,32 +1,41 @@
 import React from 'react';
-import nextSound from '../../assets/audio/next.m4a'
+import nextSound from '../../assets/audio/next.m4a';
+import nextImage from '../../assets/next.png';  // Consider importing the image for better error handling.
 
-export default function Next_component(props) {
-  function playAudio() {
-    new Audio(nextSound).play();
+export default function Next_component({socket, next, setClickEvent}) {  // Destructuring for cleaner reference
+  async function playAudio() {  // Using async/await syntax for cleaner reads
+    const audio = new Audio(nextSound);
+    try {
+      await audio.play();
+      // Automatic playback started!
+    } catch (error) {
+      // Auto-play was prevented, or there were some other issues.
+      console.error("Playback failed.", error);
+    }
   }
-  const next = () => {
 
+  const nextAction = async () => {  // Reflecting the async nature of the actions here
     const room = localStorage.getItem('roomId');
     const id = localStorage.getItem('myId');
     const credentials = {
       room,
       id,
     };
-    playAudio();
-    props.socket.current.emit('next', credentials);
-    props.next(false);
-    props.setClickEvent((x) => {
-      return !x;
-    });
+    
+    await playAudio();  // Since this is an async action, we'll await it.
+
+    socket.current.emit('next', credentials);
+    next(false);
+    setClickEvent(prevClickEvent => !prevClickEvent);  // Slightly cleaner syntax.
   };
+
   return (
     <div>
       <img
-        src={require('../../assets/next.png')}
-        alt=''
+        src={nextImage}  // Directly using the imported image ensures the path is correct.
+        alt="Go to the next item"  // A more descriptive alt attribute.
         className='next'
-        onClick={next}
+        onClick={nextAction}  // Renamed for clarity.
       />
     </div>
   );
