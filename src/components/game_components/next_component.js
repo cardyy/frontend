ab5fda9bef4f238352537c0d5c0c208b8b6ecf9f
Cardinal Ncube <52386656+cardyy@users.noreply.github.com>
2023-10-22 19:27:13 +0200

    Add files via upload

--- src/components/game_components/next_component.js
+++ src/components/game_components/next_component.js
@@ -1,32 +1,56 @@
 import React from 'react';
-import nextSound from '../../assets/audio/next.m4a'
+import nextSound from '../../assets/audio/next.m4a';
+import nextImage from '../../assets/next.png';  // Consider importing the image for better error handling.
 
-export default function Next_component(props) {
-  function playAudio() {
-    new Audio(nextSound).play();
+export default function Next_component({socket, next, setClickEvent}) {  // Destructuring for cleaner reference
+
+  // At the top of your file
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+
+// When playing the audio
+async function playAudio() {
+  try {
+    // Check if context is in suspended state (autoplay policy)
+    if (audioContext.state === 'suspended') {
+      await audioContext.resume();
+    }
+    const audio = new Audio(nextSound);
+    audio.src = nextSound;  // Ensure the source is set correctly.
+    audio.load();  // Sometimes necessary to reload the source.
+    
+    // Now, we're ensuring the audio plays via the audio context
+    const track = audioContext.createMediaElementSource(audio);
+    track.connect(audioContext.destination);
+
+    await audio.play();  // Play the audio.
+  } catch (err) {
+    console.error('Failed to play:', err);
   }
-  const next = () => {
+}
 
+  const nextAction = async () => {  // Reflecting the async nature of the actions here
     const room = localStorage.getItem('roomId');
     const id = localStorage.getItem('myId');
     const credentials = {
       room,
       id,
     };
-    playAudio();
-    props.socket.current.emit('next', credentials);
-    props.next(false);
-    props.setClickEvent((x) => {
-      return !x;
-    });
+    
+    await playAudio();  // Since this is an async action, we'll await it.
+
+    socket.current.emit('next', credentials);
+    next(false);
+    setClickEvent(prevClickEvent => !prevClickEvent);  // Slightly cleaner syntax.
   };
+
   return (
     <div>
       <img
-        src={require('../../assets/next.png')}
-        alt=''
+        src={nextImage}  // Directly using the imported image ensures the path is correct.
+        alt="Go to the next item"  // A more descriptive alt attribute.
         className='next'
-        onClick={next}
+        onClick={nextAction}  // Renamed for clarity.
       />
     </div>
   );
--- src/components/game_components/players_component.js
+++ src/components/game_components/players_component.js
@@ -1,165 +1,21 @@
-import React, { useRef, useEffect, useState } from 'react';
-import Peer from 'simple-peer';
-import io from 'socket.io-client';
-
-const Video = (props) => {
-  const ref = useRef();
-  useEffect(() => {
-    props.peer.on('stream', (stream) => {
-      ref.current.srcObject = stream;
-      ref.current.play().catch(console.error); // Handling the case where autoplay was prevented.
-    });
-  }, [props.peer]);
-  return (
-    <video playsInline ref={ref} autoPlay className='avators av-top' id={`${props.pos}`} />
-  );
-};
+import React from 'react';
 
 const Players_component = (props) => {
-  const [peers, setPeers] = useState([]);
-  const socketRef = useRef();
-  const userVideo = useRef();
-  const peersRef = useRef([]);
-  const roomID = props.roomID;
   const glow = (id) => {
     if (props.activePlayer === id) {
       return 'glow';
     }
   };
 
-  useEffect(() => {
-    for (let i = 0; i <= peers.length; i++) {
-      if (
-        props.activePlayers.length < 1 ||
-        document.getElementById(`${i}`) === null
-      )
-        return;
-      document.getElementById(`${i}${i}`).style.position = 'absolute';
-      document.getElementById(`${i}${i}`).style.display = 'block';
-      document.getElementById(`${i}${i}`).style.left =
-        document.getElementById(`${i}`).offsetLeft + 'px';
-      document.getElementById(`${i}${i}`).style.top =
-        document.getElementById(`${i}`).offsetTop + 'px';
-    }
-  }, [peers.length, props.activePlayers.length]);
-
-  useEffect(() => {
-    socketRef.current = io.connect('https://crazy-eights-f5fce4747505.herokuapp.com');
-    navigator.mediaDevices
-      .getUserMedia({ video: false, audio: true })
-      .then((stream) => {
-        userVideo.current.srcObject = stream;
-        socketRef.current.emit('join room', roomID);
-        socketRef.current.on('all users', (users) => {
-          const peers = [];
-          users.forEach((userID) => {
-            const peer = createPeer(userID, socketRef.current.id, stream);
-            peersRef.current.push({
-              peerID: userID,
-              peer,
-            });
-            peers.push({
-              peerID: userID,
-              peer,
-            });
-          });
-          setPeers(peers);
-        });
-
-        socketRef.current.on('user joined', (payload) => {
-          const peer = addPeer(payload.signal, payload.callerID, stream);
-          peersRef.current.push({
-            peerID: payload.callerID,
-            peer,
-          });
-
-          const peerObj = {
-            peer,
-            peerID: payload.callerID,
-          };
-
-          setPeers((users) => [...users, peerObj]);
-        });
-
-        socketRef.current.on('receiving returned signal', (payload) => {
-          const item = peersRef.current.find((p) => p.peerID === payload.id);
-          item.peer.signal(payload.signal);
-        });
-
-        socketRef.current.on('user left', (id) => {
-          const peerObj = peersRef.current.find((p) => p.peerID === id);
-          if (peerObj) {
-            peerObj.peer.destroy();
-          }
-          const peers = peersRef.current.filter((p) => p.peerID !== id);
-          peersRef.current = peers;
-          setPeers(peers);
-        });
-      });
-  }, []);
-
-  function createPeer(userToSignal, callerID, stream) {
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream,
-    });
-
-    peer.on('signal', (signal) => {
-      socketRef.current.emit('sending signal', {
-        userToSignal,
-        callerID,
-        signal,
-      });
-    });
-
-    return peer;
-  }
-
-  function addPeer(incomingSignal, callerID, stream) {
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream,
-    });
-
-    peer.on('signal', (signal) => {
-      socketRef.current.emit('returning signal', { signal, callerID });
-    });
-
-    peer.signal(incomingSignal);
-
-    return peer;
-  }
+  console.log('props.activePlayers',props.activePlayers)
 
   return (
-    <div className='players'>
-      <video
-        playsInline
-        muted
-        ref={userVideo}
-        autoPlay
-        className='avators av-top'
-        id='0'
-      />
-
-      {peers
-        .filter(
-          (v, i, a) => a.findLastIndex((v2) => v2.peerID === v.peerID) === i
-        )
-        .map((peer, index) => {
-          return (
-            <Video
-              key={peer.peerID}
-              peer={peer.peer}
-              pos={() => {
-                return index + 1;
-              }}
-            />
-          );
-        })}
-      {props.activePlayers.map((playerId, index) => (
-        <div className={`${glow(playerId)} av-bot`} id={`${index}${index}`} key={index} />
+    <div >
+      {props.activePlayers.map(player => (
+        <div key={player.id} className="av-container">
+          <div className={`${glow(player.id)} av-bot`} id={`player-${player.id}`} /> 
+          <h4>{player.username}: {player.playerHand.length} cards</h4> {}
+        </div>
       ))}
     </div>
   );
