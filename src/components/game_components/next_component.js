c2940341ad28575a6bb0da12dc04e6b010961cd0
Cardinal Ncube <52386656+cardyy@users.noreply.github.com>
2023-10-22 19:46:46 +0200

    Add files via upload

--- src/components/game_components/next_component.js
+++ src/components/game_components/next_component.js
@@ -1,56 +1,64 @@
 import React from 'react';
 import nextSound from '../../assets/audio/next.m4a';
-import nextImage from '../../assets/next.png';  // Consider importing the image for better error handling.
+import nextImage from '../../assets/next.png';
+import { Howl } from 'howler';
 
-export default function Next_component({socket, next, setClickEvent}) {  // Destructuring for cleaner reference
+export default function Next_component({ socket, next, setClickEvent }) {
+  // We're using Howler for audio because it handles a lot of cross-browser
+  // and cross-device issues for us. Create a Howl instance for our sound.
+  const sound = new Howl({
+    src: [nextSound],
+    preload: true, // Preload the sound for better performance.
+    html5: true, // Use HTML5 Audio API for wider device compatibility, especially on mobile.
+  });
 
-  // At the top of your file
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioContext = new AudioContext();
+  // Function to handle the actual audio playing
+  const playAudio = async () => {
+    try {
+      // Attempt to play the sound.
+      const soundId = sound.play();
 
-// When playing the audio
-async function playAudio() {
-  try {
-    // Check if context is in suspended state (autoplay policy)
-    if (audioContext.state === 'suspended') {
-      await audioContext.resume();
+      // If you want to do more with the sound, you can reference it with the soundId, e.g., control volume, etc.
+    } catch (err) {
+      console.error('Failed to play the sound:', err);
     }
-    const audio = new Audio(nextSound);
-    audio.src = nextSound;  // Ensure the source is set correctly.
-    audio.load();  // Sometimes necessary to reload the source.
-    
-    // Now, we're ensuring the audio plays via the audio context
-    const track = audioContext.createMediaElementSource(audio);
-    track.connect(audioContext.destination);
+  };
 
-    await audio.play();  // Play the audio.
-  } catch (err) {
-    console.error('Failed to play:', err);
-  }
-}
+  // Function to handle the "next" action.
+  const nextAction = async () => {
+    try {
+      // Play the audio first; wait for it to finish if necessary.
+      await playAudio();
 
-  const nextAction = async () => {  // Reflecting the async nature of the actions here
-    const room = localStorage.getItem('roomId');
-    const id = localStorage.getItem('myId');
-    const credentials = {
-      room,
-      id,
-    };
-    
-    await playAudio();  // Since this is an async action, we'll await it.
+      // Get the room and ID from localStorage.
+      const room = localStorage.getItem('roomId');
+      const id = localStorage.getItem('myId');
 
-    socket.current.emit('next', credentials);
-    next(false);
-    setClickEvent(prevClickEvent => !prevClickEvent);  // Slightly cleaner syntax.
+      // Build the credentials object.
+      const credentials = {
+        room,
+        id,
+      };
+
+      // Communicate with the server.
+      socket.current.emit('next', credentials);
+
+      // Update state and UI components.
+      next(false);
+      setClickEvent((prevClickEvent) => !prevClickEvent);
+
+    } catch (error) {
+      console.error('Error during next action:', error);
+    }
   };
 
   return (
     <div>
       <img
-        src={nextImage}  // Directly using the imported image ensures the path is correct.
-        alt="Go to the next item"  // A more descriptive alt attribute.
-        className='next'
-        onClick={nextAction}  // Renamed for clarity.
+        src={nextImage}
+        alt="Next"
+        className="next"
+        onClick={nextAction} // Direct interaction starts audio playback, respecting browser restrictions.
       />
     </div>
   );
