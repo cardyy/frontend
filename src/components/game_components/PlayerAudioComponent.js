5e953d08a138e6a7a82dac8c5f2a1ae270dcc703
Cardinal Ncube <52386656+cardyy@users.noreply.github.com>
2023-10-23 08:41:59 +0200

    Add files via upload

--- src/components/game_components/PlayerAudioComponent.js
+++ src/components/game_components/PlayerAudioComponent.js
@@ -1,76 +1,92 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Peer from 'simple-peer'; // A simple library to work with WebRTC.
-import io from 'socket.io-client'; // Real-time library
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMicrophone, faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useRef, useState } from "react";
+import Peer from "simple-peer"; // A simple library to work with WebRTC.
+import io from "socket.io-client"; // Real-time library
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMicrophone,
+  faMicrophoneSlash,
+} from "@fortawesome/free-solid-svg-icons";
 
-const PlayerAudioComponent = ({ roomID,playerId,id }) => { // Assuming you want to use a room-based system
+const PlayerAudioComponent = ({ roomID, playerId, id }) => {
+  // Assuming you want to use a room-based system
   const [isMuted, setIsMuted] = useState(false);
   const [peers, setPeers] = useState([]);
   const socketRef = useRef();
   const audioRef = useRef();
   const peersRef = useRef([]);
+  const [userAudioEnabled, setUserAudioEnabled] = useState(true);
 
   useEffect(() => {
     // Connect to the signaling server (socket.io server)
-    socketRef.current = io.connect('https://crazy-eights-f5fce4747505.herokuapp.com');
+    socketRef.current = io.connect(
+      "https://crazy-eights-f5fce4747505.herokuapp.com",
+    );
 
     // Request access to the user's audio
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then(stream => {
-        audioRef.current.srcObject = stream;
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      audioRef.current.srcObject = stream;
 
-        // Join a room
-        socketRef.current.emit('join room', roomID);
-        
-        socketRef.current.on('all users', (users) => {
-          const peers = [];
-          users.forEach((userID) => {
-            const peer = createPeer(userID, socketRef.current.id, stream);
-            peersRef.current.push({
-              peerID: userID,
-              peer,
-            });
-            peers.push(peer);
-          });
-          setPeers(peers);
-        });
+      // Join a room
+      socketRef.current.emit("join room", roomID);
 
-        socketRef.current.on('user joined', (payload) => {
-          const peer = addPeer(payload.signal, payload.callerID, stream);
+      socketRef.current.on("all users", (users) => {
+        const peers = [];
+        users.forEach((userID) => {
+          const peer = createPeer(userID, socketRef.current.id, stream);
           peersRef.current.push({
-            peerID: payload.callerID,
+            peerID: userID,
             peer,
           });
-
-          setPeers(users => [...users, peer]);
+          peers.push(peer);
         });
-
-        socketRef.current.on('receiving returned signal', (payload) => {
-          const item = peersRef.current.find(p => p.peerID === payload.id);
-          item.peer.signal(payload.signal);
-        });
-
-        socketRef.current.on('user left', (id) => {
-          const peerObj = peersRef.current.find(p => p.peerID === id);
-          if(peerObj) {
-            peerObj.peer.destroy();
-          }
-          const peers = peersRef.current.filter(p => p.peerID !== id);
-          peersRef.current = peers;
-          setPeers(peers);
-        });
+        setPeers(peers);
       });
+
+      socketRef.current.on("user joined", (payload) => {
+        const peer = addPeer(payload.signal, payload.callerID, stream);
+        peersRef.current.push({
+          peerID: payload.callerID,
+          peer,
+        });
+
+        setPeers((users) => [...users, peer]);
+      });
+
+      socketRef.current.on("receiving returned signal", (payload) => {
+        const item = peersRef.current.find((p) => p.peerID === payload.id);
+        item.peer.signal(payload.signal);
+      });
+
+      socketRef.current.on("user left", (id) => {
+        const peerObj = peersRef.current.find((p) => p.peerID === id);
+        if (peerObj) {
+          peerObj.peer.destroy();
+        }
+        const peers = peersRef.current.filter((p) => p.peerID !== id);
+        peersRef.current = peers;
+        setPeers(peers);
+      });
+
+      const audioTracks = stream.getAudioTracks();
+      if (audioTracks.length > 0) {
+        audioTracks[0].enabled = userAudioEnabled;
+      }
+    });
 
     function createPeer(userToSignal, callerID, stream) {
       const peer = new Peer({
         initiator: true,
         trickle: false,
         stream,
+        config: { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] }
       });
 
-      peer.on('signal', signal => {
-        socketRef.current.emit('sending signal', { userToSignal, callerID, signal });
+      peer.on("signal", (signal) => {
+        socketRef.current.emit("sending signal", {
+          userToSignal,
+          callerID,
+          signal,
+        });
       });
 
       return peer;
@@ -81,10 +97,11 @@ const PlayerAudioComponent = ({ roomID,playerId,id }) => { // Assuming you want 
         initiator: false,
         trickle: false,
         stream,
+        config: { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] }
       });
 
-      peer.on('signal', signal => {
-        socketRef.current.emit('returning signal', { signal, callerID });
+      peer.on("signal", (signal) => {
+        socketRef.current.emit("returning signal", { signal, callerID });
       });
 
       peer.signal(incomingSignal);
@@ -101,50 +118,65 @@ const PlayerAudioComponent = ({ roomID,playerId,id }) => { // Assuming you want 
     };
   }, []);
 
- 
   const handleMuteToggle = () => {
-    const enabled = audioRef.current.srcObject.getAudioTracks()[0].enabled;
-    if (enabled) {
-      audioRef.current.srcObject.getAudioTracks()[0].enabled = false;
-      setIsMuted(true);
-    } else {
-      audioRef.current.srcObject.getAudioTracks()[0].enabled = true;
-      setIsMuted(false);
-    }
-  };
+    setUserAudioEnabled((prevEnabled) => {
+      const newEnabledState = !prevEnabled;
 
+      // Get our own stream and update it.
+      if (audioRef.current && audioRef.current.srcObject) {
+        const audioTracks = audioRef.current.srcObject.getAudioTracks();
+        if (audioTracks.length > 0) {
+          // Enable or disable the track depending on the new state.
+          audioTracks[0].enabled = newEnabledState;
+        }
+      }
+
+      // Update the mute state.
+      setIsMuted(!newEnabledState);
+      return newEnabledState;
+    });
+  };
 
   return (
     <>
       <div className="player-audio">
         <audio ref={audioRef} autoPlay playsInline></audio>
-        {playerId === id && (<button onClick={handleMuteToggle} style={{ background: 'none', border: 'none', cursor: 'pointer', padding: '10px', color: 'white' }}>
+        <button
+          onClick={handleMuteToggle}
+          style={{
+            background: "none",
+            border: "none",
+            cursor: "pointer",
+            padding: "10px",
+            color: "white",
+          }}
+        >
           {isMuted ? (
             <FontAwesomeIcon icon={faMicrophoneSlash} size="2x" />
           ) : (
             <FontAwesomeIcon icon={faMicrophone} size="2x" />
           )}
-        </button>)}
+        </button>
       </div>
-      {peers.map((peer) => {
-        return (
-          <Audio key={peer.id} peer={peer} />
-        );
+      {peers.map((peer, index) => {
+        return <Audio key={index} peer={peer} isMuted={isMuted} />;
       })}
     </>
   );
 };
 
-const Audio = ({ peer }) => {
+const Audio = ({ peer, isMuted }) => {
   const ref = useRef();
 
   useEffect(() => {
-    peer.on('stream', stream => {
+    peer.on("stream", (stream) => {
       ref.current.srcObject = stream;
+      // Here, you could also adjust the audio track enabled/disabled state based on `isMuted`.
     });
-  }, [peer]);
+  }, [peer, isMuted]); // Make sure to react to changes of isMuted.
 
-  return <audio playsInline autoPlay ref={ref} />;
+  // Adjust audio element based on mute state.
+  return <audio playsInline autoPlay ref={ref} muted={isMuted} />;
 };
 
 export default PlayerAudioComponent;
