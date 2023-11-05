e42e0c9afad74f23f224ba59cf1bc672cc50e0b4
Cardinal Ncube <52386656+cardyy@users.noreply.github.com>
2023-11-05 10:51:15 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Game from './components/game_components/game_component';
-import './App.css';
+import React from 'react'
+import Game from './components/game_components/game_component'
+import './App.css'
 
 export default function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Game />
     </div>
-  );
+  )
 }
